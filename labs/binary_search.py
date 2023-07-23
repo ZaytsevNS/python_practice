7a52ee48daba00f7e0ccf279ccdc0c9cfba1f529
Nickolay Zaytsev <fatalbest@mail.ru>
2023-07-23 23:57:10 +0300

    Add type to var

--- labs/binary_search.py
+++ labs/binary_search.py
@@ -1,6 +1,6 @@
 def binary_search(list, item):
-    low = 0
-    high = len(list)-1
+    low: int = 0
+    high: int = len(list)-1
     
     while low <= high:
         mid = (low+high)//2
@@ -13,5 +13,5 @@ def binary_search(list, item):
             low = mid + 1
     return None
 
-my_list = [1, 3, 5, 7, 9, 11]
+my_list: list = [1, 3, 5, 7, 9, 11]
 print(binary_search(my_list, 9))
